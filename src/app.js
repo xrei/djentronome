4d3bb9348452d04dbf9defa44acd6d9232178d1e
xrei <romanrei.dev@gmail.com>
2018-04-30 04:27:42 +0300

    Change meta description

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ export function createApp () {
     metaInfo: {
       title: 'Metronome',
       meta: [
-        { vmid: 'description', name: 'description', content: 'Free online metronome app for practice with support of odd time signatures' },
+        { name: 'description', content: 'Free online metronome app for practice with support of odd time signatures' },
         { name: 'keywords', content: 'metronome, djentronome, app, online, timing, practice, guitar, music, vue' }
       ],
       titleTemplate: '%s | Djentronome',
