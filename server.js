8ef7a60dd7a2589d69b0eeba6448be09bd208f8f
xrei <romanrei.dev@gmail.com>
2018-05-17 04:37:55 +0300

    style: lint

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ function render (req, res) {
   const handleError = err => {
     if (err.url) {
       res.redirect(err.url)
-    } else if(err.code === 404) {
+    } else if (err.code === 404) {
       res.status(404).send('404 | Page Not Found')
     } else {
       // Render Error Page or Redirect
